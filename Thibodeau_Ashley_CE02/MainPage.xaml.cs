47a149b041cdcaeede1145cd9fb6fb987d84faee
Ashley Thibodeau <AThibodeau1@Student.Fullsail.edu>
2021-02-06 20:28:29 -0500

    added buttons to main page added file saving

--- Thibodeau_Ashley_CE02/MainPage.xaml.cs
+++ Thibodeau_Ashley_CE02/MainPage.xaml.cs
@@ -12,12 +12,45 @@ namespace Thibodeau_Ashley_CE02
     public partial class MainPage : ContentPage
     {
         private StudentData passedStudentData = new StudentData();
+
         public MainPage()
         {
             InitializeComponent();
 
+            if(loadButton.IsVisible != true)
+            {
+                saveButton.Margin = new Thickness(0,0,130,0);
+            }
+
+            saveButton.Clicked += SaveButton_Clicked;
+            clearButton.Clicked += ClearButton_Clicked;
+            loadButton.Clicked += LoadButton_Clicked;
             addButton.Clicked += AddButton_Clicked;
         }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            if (passedStudentData.Name != null)
+            {
+                passedStudentData.SaveFile(passedStudentData.Name, passedStudentData.Age, passedStudentData.StartDate, passedStudentData.Active);
+
+                saveButton.Margin = new Thickness(0, 0, 20, 0);
+                loadButton.IsVisible = true;
+            }
+
+        }
+
+        private void LoadButton_Clicked(object sender, EventArgs e)
+        {
+            passedStudentData = passedStudentData.LoadFile();
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+
 
         protected override void OnAppearing()
         {
--- Thibodeau_Ashley_CE02/Models/StudentData.cs
+++ Thibodeau_Ashley_CE02/Models/StudentData.cs
@@ -6,14 +6,45 @@
     GitHub Link: https://github.com/InterfaceProgramming/ce2-ThibodeauAshley-FS
  */
 using System;
+using System.IO;
 
 namespace Thibodeau_Ashley_CE02.Models
 {
     public class StudentData
     {
+        private string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CE02_Data.txt");
+
+
         public string Name { get; set; }
         public Double Age { get; set; }
         public DateTime StartDate { get; set; }
         public bool Active { get; set; }
+
+        public void SaveFile(string name, double age, DateTime startDate, bool activeStatus)
+        {
+            using (var sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine(name);
+                sw.WriteLine(age);
+                sw.WriteLine(startDate.ToString());
+                sw.WriteLine(activeStatus.ToString());
+            }
+
+        }
+
+        public StudentData LoadFile()
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+
+            }
+
+            return this;
+        }
+
+
     }
+
+
+
 }
